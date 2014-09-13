c72b7ca7946df98c09b4e24450dde7829f582627
Mitar <mitar.git@tnode.com>
2014-09-12 22:43:19 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,20 +1,10 @@
 Package.describe({
   summary: "Cryptographic digest and HMAC algorithms in Javascript for modern web browsers",
   version: '0.1.3',
-  name: 'peerlibrary:digest',
+  name: 'mrt:digest',
   git: 'https://github.com/peerlibrary/meteor-digest.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
-  api.export('Digest');
-
-  api.add_files([
-    'digest/digest.js'
-  ], 'client', {bare: true});
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:digest', 'tinytest', 'test-helpers'], 'client');
-  api.add_files('tests.js', 'client');
+  api.imply('peerlibrary:digest@0.1.3');
 });
